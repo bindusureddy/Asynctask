e8dbd02b81d251d72a6bd3f619435b315f045eb4
sureddybindureddy <sureddybindureddy@gmail.com>
2018-05-15 16:01:47 +0530

    Initial commit

---
+++ app/src/main/java/bindu/asynctask/MainActivity.java
@@ -0,0 +1,143 @@
+package bindu.asynctask;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    String Url="http://mobileappdatabase.in/demo/smartnews/app_dashboard/jsonUrl/single-article.php?article-id=71";
+    String title ,image,category;
+    TextView titleTextView,categoryTextView;
+    ProgressDialog progressDialog;
+    Button displayData;
+    ImageView imageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    titleTextView =(TextView)findViewById(R.id.titleTextView);
+    categoryTextView = (TextView)findViewById(R.id.categoryTextView);
+    displayData = (Button)findViewById(R.id.displayData);
+    imageView = (ImageView)findViewById(R.id.imageView);
+
+
+
+
+displayData.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        MyAsyncTasks myAsyncTask = new MyAsyncTasks();
+        myAsyncTask.execute();
+    }
+});
+    }
+    public class MyAsyncTasks extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // display a progress dialog for good user experiance
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setMessage("Please Wait");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            // implement API in background and store the response in current variable
+            String current = "";
+            try {
+                URL url;
+                HttpURLConnection urlConnection = null;
+                try {
+                    url = new URL(Url);
+
+                    urlConnection = (HttpURLConnection) url
+                            .openConnection();
+
+                    InputStream in = urlConnection.getInputStream();
+
+                    InputStreamReader isw = new InputStreamReader(in);
+
+                    int data = isw.read();
+                    while (data != -1) {
+                        current += (char) data;
+                        data = isw.read();
+                        System.out.print(current);
+
+                    }
+                    // return the data to onPostExecute method
+                    return current;
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Exception: " + e.getMessage();
+            }
+            return current;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            Log.d("data", s.toString());
+            // dismiss the progress dialog after receiving data from API
+            progressDialog.dismiss();
+            try {
+                // JSON Parsing of data
+                JSONArray jsonArray = new JSONArray(s);
+
+                JSONObject oneObject = jsonArray.getJSONObject(0);
+                // Pulling items from the array
+                title = oneObject.getString("title");
+                category = oneObject.getString("category");
+                image = oneObject.getString("image");
+                // display the data in UI
+                titleTextView.setText("Title: "+title);
+                categoryTextView.setText("Category: "+category);
+                // Picasso library to display the image from URL
+                Picasso.with(getApplicationContext())
+                        .load(image)
+                        .into(imageView);
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+    }
+}
